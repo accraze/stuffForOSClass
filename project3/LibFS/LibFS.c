c3389e1fa577711a786bbc174690d7de0e4a6b18
accraze <accraze@gmail.com>
2014-11-11 17:17:21 -0700

    cleaned up LibFS

--- project3/LibFS/LibFS.c
+++ project3/LibFS/LibFS.c
@@ -1,11 +1,6 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1038\cocoasubrtf360
-{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww9000\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\ql\qnatural
 
-\f0\fs24 \cf0 #include "LibFS.h"\
+
+#include "LibFS.h"\
 #include "LibDisk.h"\
 \
 // global errno value here\
@@ -17,19 +12,18 @@ FS_Boot(char *path)\
     printf("FS_Boot %s\\n", path);\
 \
     // oops, check for errors\
-    if (Disk_Init() == -1) \{\
-	printf("Disk_Init() failed\\n");\
-	osErrno = E_GENERAL;\
-	return -1;\
-    \}\
-\
-    // do all of the other stuff needed...\
-\
-    return 0;\
-\}\
-\
-int\
-FS_Sync()\
+    if (Disk_Init() == -1) \{
+	printf("Disk_Init() failed\\n");
+	osErrno = E_GENERAL;
+	return -1;
+    }
+
+
+    return 0;
+}
+
+int
+FS_Sync()
 \{\
     printf("FS_Sync\\n");\
     return 0;\
