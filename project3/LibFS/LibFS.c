82edab8e2b074e6a68fbc49946a4f7a04fad354d
accraze <accraze@gmail.com>
2014-11-16 10:59:06 -0700

    starting to ascent

--- project3/LibFS/LibFS.c
+++ project3/LibFS/LibFS.c
@@ -1,110 +1,115 @@
+#include "LibDisk.h"
 
+// global errno value here
+int osErrno;
 
-#include "LibFS.h"\
-#include "LibDisk.h"\
-\
-// global errno value here\
-int osErrno;\
-\
-int \
-FS_Boot(char *path)\
-\{\
-    printf("FS_Boot %s\\n", path);\
-\
-    // oops, check for errors\
-    if (Disk_Init() == -1) \{
-	printf("Disk_Init() failed\\n");
+int 
+FS_Boot(char *path)
+{
+    printf("FS_Boot %sn", path);
+
+    // oops, check for errors
+    if (Disk_Init() == -1) {
+	printf("Disk_Init() failedn");
 	osErrno = E_GENERAL;
 	return -1;
     }
 
+    //now search for path
+    int check = File_Open(path);
+    if(check == -1){
+        //if not found
+    File_Create();
+    }
+
+    // do all of the other stuff needed...
 
     return 0;
 }
 
 int
 FS_Sync()
-\{\
-    printf("FS_Sync\\n");\
-    return 0;\
-\}\
-\
-\
-int\
-File_Create(char *file)\
-\{\
-    printf("FS_Create\\n");\
-    return 0;\
-\}\
-\
-int\
-File_Open(char *file)\
-\{\
-    printf("FS_Open\\n");\
-    return 0;\
-\}\
-\
-int\
-File_Read(int fd, void *buffer, int size)\
-\{\
-    printf("FS_Read\\n");\
-    return 0;\
-\}\
-\
-int\
-File_Write(int fd, void *buffer, int size)\
-\{\
-    printf("FS_Write\\n");\
-    return 0;\
-\}\
-\
-int\
-File_Seek(int fd, int offset)\
-\{\
-    printf("FS_Seek\\n");\
-    return 0;\
-\}\
-\
-int\
-File_Close(int fd)\
-\{\
-    printf("FS_Close\\n");\
-    return 0;\
-\}\
-\
-int\
-File_Unlink(char *file)\
-\{\
-    printf("FS_Unlink\\n");\
-    return 0;\
-\}\
-\
-\
-// directory ops\
-int\
-Dir_Create(char *path)\
-\{\
-    printf("Dir_Create %s\\n", path);\
-    return 0;\
-\}\
-\
-int\
-Dir_Size(char *path)\
-\{\
-    printf("Dir_Size\\n");\
-    return 0;\
-\}\
-\
-int\
-Dir_Read(char *path, void *buffer, int size)\
-\{\
-    printf("Dir_Read\\n");\
-    return 0;\
-\}\
-\
-int\
-Dir_Unlink(char *path)\
-\{\
-    printf("Dir_Unlink\\n");\
-    return 0;\
-\}}
+{
+    printf("FS_Sync\n");
+    return 0;
+}
+
+
+int
+File_Create(char *file)
+{
+    printf("FS_Create\n");
+    return 0;
+}
+
+int
+File_Open(char *file)
+{
+    printf("FS_Openn");
+    return 0;
+}
+
+int
+File_Read(int fd, void *buffer, int size)
+{
+    printf("FS_Readn");
+    return 0;
+}
+
+int
+File_Write(int fd, void *buffer, int size)
+{
+    printf("FS_Writen");
+    return 0;
+}
+
+int
+File_Seek(int fd, int offset)
+{
+    printf("FS_Seekn");
+    return 0;
+}
+
+int
+File_Close(int fd)
+{
+    printf("FS_Closen");
+    return 0;
+}
+
+int
+File_Unlink(char *file)
+{
+    printf("FS_Unlinkn");
+    return 0;
+}
+
+
+// directory ops
+int
+Dir_Create(char *path)
+{
+    printf("Dir_Create %sn", path);
+    return 0;
+}
+
+int
+Dir_Size(char *path)
+{
+    printf("Dir_Sizen");
+    return 0;
+}
+
+int
+Dir_Read(char *path, void *buffer, int size)
+{
+    printf("Dir_Readn");
+    return 0;
+}
+
+int
+Dir_Unlink(char *path)
+{
+    printf("Dir_Unlinkn");
+    return 0;
+}}
--- project3/LibFS/LibFS.h
+++ project3/LibFS/LibFS.h
@@ -1,58 +1,50 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1038\cocoasubrtf360
-{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww9000\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\ql\qnatural
+#define __LibFS_h__
 
-\f0\fs24 \cf0 #ifndef __LibFS_h__\
-#define __LibFS_h__\
-\
-/*\
- * DO NOT MODIFY THIS FILE\
- */\
-    \
-#include <stdio.h>\
-#include <stdlib.h>\
-#include <unistd.h>\
-\
-// used for errors\
-extern int osErrno;\
-    \
-// error types - don't change anything about these!! (even the order!)\
-typedef enum \{\
-    E_GENERAL,      // general\
-    E_CREATE, \
-    E_NO_SUCH_FILE, \
-    E_TOO_MANY_OPEN_FILES, \
-    E_BAD_FD, \
-    E_NO_SPACE, \
-    E_FILE_TOO_BIG, \
-    E_SEEK_OUT_OF_BOUNDS, \
-    E_FILE_IN_USE, \
-    E_BUFFER_TOO_SMALL, \
-    E_DIR_NOT_EMPTY,\
-    E_ROOT_DIR,\
-\} FS_Error_t;\
-    \
-// File system generic call\
-int FS_Boot(char *path);\
-int FS_Sync();\
-\
-// file ops\
-int File_Create(char *file);\
-int File_Open(char *file);\
-int File_Read(int fd, void *buffer, int size);\
-int File_Write(int fd, void *buffer, int size);\
-int File_Seek(int fd, int offset);\
-int File_Close(int fd);\
-int File_Unlink(char *file);\
-\
-// directory ops\
-int Dir_Create(char *path);\
-int Dir_Size(char *path);\
-int Dir_Read(char *path, void *buffer, int size);\
-int Dir_Unlink(char *path);\
-\
-#endif /* __LibFS_h__ */\
+/*
+ * DO NOT MODIFY THIS FILE
+ */
+    
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// used for errors
+extern int osErrno;
+    
+// error types - don't change anything about these!! (even the order!)
+typedef enum {
+    E_GENERAL,      // general
+    E_CREATE, 
+    E_NO_SUCH_FILE, 
+    E_TOO_MANY_OPEN_FILES, 
+    E_BAD_FD, 
+    E_NO_SPACE, 
+    E_FILE_TOO_BIG, 
+    E_SEEK_OUT_OF_BOUNDS, 
+    E_FILE_IN_USE, 
+    E_BUFFER_TOO_SMALL, 
+    E_DIR_NOT_EMPTY,
+    E_ROOT_DIR,
+} FS_Error_t;
+    
+// File system generic call
+int FS_Boot(char *path);
+int FS_Sync();
+
+// file ops
+int File_Create(char *file);
+int File_Open(char *file);
+int File_Read(int fd, void *buffer, int size);
+int File_Write(int fd, void *buffer, int size);
+int File_Seek(int fd, int offset);
+int File_Close(int fd);
+int File_Unlink(char *file);
+
+// directory ops
+int Dir_Create(char *path);
+int Dir_Size(char *path);
+int Dir_Read(char *path, void *buffer, int size);
+int Dir_Unlink(char *path);
+
+#endif /* __LibFS_h__ */
 }
